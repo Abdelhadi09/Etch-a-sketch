3748bea53e197d40aef69b16e1c5303ac8b031ec
abdelhadi <abaditrki@gmail.com>
2024-09-01 16:36:19 -0700

    some refactorisation

--- app.js
+++ app.js
@@ -1,48 +1,46 @@
-let container = document.getElementById("container")
-let range = document.querySelector("input")
-range.addEventListener("mousemove", (e) => updateSizeValue(e.target.value))
-range.onchange = (e) => makeGrid(e.target.value)
+// initialize variables
+let container = document.getElementById("container");
+let range = document.querySelector("input");
+let clear = document.getElementById("clear");
+let width = container.clientWidth;
+let mouseDown = false;
+// functions
 function updateSizeValue(value) {
-    document.getElementById("sizeValue").innerHTML = `${value} x ${value}`
+  document.getElementById("sizeValue").innerHTML = `${value} x ${value}`;
 }
-
 function makeGrid(size) {
-    container.innerHTML = ""
-    for (let i = 0; i < size*size ; i++) {
-    let div = document.createElement("div")
-    div.classList.add("square")
-   
-    let width = container.clientWidth
-    let squareWidth = width/size
-   
-    div.style.width = `${squareWidth}px`
-    div.style.height = `${squareWidth}px`
-     container.appendChild(div)
-let squares = document.querySelectorAll(".square")
-squares.forEach((square) => {
-    square.addEventListener("mousedown", changeColor)
-    square.addEventListener("mouseover", changeColor)
-})} 
-
-}
-makeGrid()
-let mouseDown = false
-document.addEventListener("mousedown", () => (mouseDown = true))
-document.addEventListener("mouseup", () => (mouseDown = false))
-function changeColor(e) {
-    if (e.type === "mouseover") {
-        if (mouseDown) {
-            e.target.style.backgroundColor = "black"
-            console.log("mouse is down")
-        }
-    }
-}
-
-let clear = document.getElementById("clear")
-clear.addEventListener("click", () => {
-    let squares = document.querySelectorAll(".square")
+  container.innerHTML = "";
+  for (let i = 0; i < size * size; i++) {
+    let div = document.createElement("div");
+    div.classList.add("square");
+    let squareWidth = width / size;
+    div.style.width = `${squareWidth}px`;
+    div.style.height = `${squareWidth}px`;
+    container.appendChild(div);
+    let squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
-        square.style.backgroundColor = "white"
-    })
-})
-
+      square.addEventListener("mousedown", changeColor);
+      square.addEventListener("mouseover", changeColor);
+    });
+  }
+}
+function changeColor(e) {
+  if (e.type === "mouseover") {
+    if (mouseDown) {
+      e.target.style.backgroundColor = "black";
+    }
+  }
+}
+function clearGrid() {
+  let squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.style.backgroundColor = "white";
+  });
+}
+// program
+makeGrid(16);
+clear.addEventListener("click", () => clearGrid());
+document.addEventListener("mousedown", () => (mouseDown = true));
+document.addEventListener("mouseup", () => (mouseDown = false));
+range.addEventListener("mousemove", (e) => updateSizeValue(e.target.value));
+range.onchange = (e) => makeGrid(e.target.value);
